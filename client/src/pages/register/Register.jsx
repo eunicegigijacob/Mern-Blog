02aba568ac83d7754087a6c3113aa86e4704f3f5
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-05-11 13:26:59 +0100

    edit links

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,24 +1,27 @@
-import axios from "axios";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import "./register.css";
+import axios from 'axios';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './register.css';
 
 export default function Register() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("/auth/register", {
-        username,
-        email,
-        password,
-      });
-      res.data && window.location.replace("/login");
+      const res = await axios.post(
+        'https://blog-h5nf.onrender.com/api/auth/register',
+        {
+          username,
+          email,
+          password,
+        }
+      );
+      res.data && window.location.replace('/login');
     } catch (err) {
       setError(true);
     }
@@ -57,7 +60,11 @@ export default function Register() {
           Login
         </Link>
       </button>
-      {error && <span style={{color:"red", marginTop:"10px"}}>Something went wrong!</span>}
+      {error && (
+        <span style={{ color: 'red', marginTop: '10px' }}>
+          Something went wrong!
+        </span>
+      )}
     </div>
   );
 }
